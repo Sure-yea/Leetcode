9dd330109e05125038def35d19552ed7ae9179cd
Shaurya <shaurya27112007@gmail.com>
2026-08-01 22:14:18 +0530

    Time: 0 ms (100%), Space: 46.9 MB (78.7%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
@@ -2,47 +2,6 @@ class Solution {
     public int maximumCount(int[] nums) {
         int lhs=0,rhs=nums.length-1;
         int mid=(lhs+rhs)/2;
-        
-        // while(lhs<=rhs){        //find end of 0
-        //     mid=(lhs+rhs)/2;
-        //     if(nums[mid]<0){
-        //         lhs=mid+1;
-        //     }
-        //     else if(nums[mid]>0){
-        //         rhs=mid-1;
-        //     }
-        //     else{ //nums[mid]==0
-        //         lhs=mid+1;          
-        //     }
-        // }
-        // int Pstart=mid;
-        
-        // int pos=nums.length-Pstart;
-        
-        // int mid2=mid;
-        // int lhs2=lhs,rhs2=rhs;
-
-        // while(lhs2<=rhs2){
-        //     mid2=(lhs2+rhs2)/2;
-        //     if(nums[mid2]<0){
-        //         lhs2=mid2+1;
-        //     }
-        //     else if(nums[mid2]>0){
-        //         rhs=mid2-1;
-        //     }
-        //     else{ //nums[mid]==0
-        //         rhs=mid-1;          
-        //     }
-        // }
-        // int Nend=mid2;
-        // int neg=Nend;
-
-
-        // System.out.println(Pstart);
-        // System.out.println(Nend);
-        // System.out.println(pos+" "+neg);
-        // return(Math.max(pos,neg));
-
 
         while(lhs<=rhs){        //find left of 0s
             mid=(lhs+rhs)/2;
@@ -50,11 +9,11 @@ class Solution {
                 lhs=mid+1;
             }
 
-            else{ //nums[mid]==0          go left
+            else{ //nums[mid]>=0          go left
                 rhs=mid-1;          
             }
         }
-        int negatives = lhs;
+        int neg = lhs;
 
 
         int lhs2=0,rhs2=nums.length-1;
@@ -69,12 +28,10 @@ class Solution {
                 lhs2=mid2+1;          
             }
         }
-        int positives = nums.length - lhs2;
-        return (Math.max(positives,negatives));
+        int pos=nums.length-lhs2;
 
 
-        
-
+        return (Math.max(pos,neg));
 
     }
 }
