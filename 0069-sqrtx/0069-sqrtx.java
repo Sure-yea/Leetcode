0541365c6bab32d3abf624999ed3cd495088c68c
Shaurya <shaurya27112007@gmail.com>
2026-08-02 18:51:03 +0530

    Time: 1 ms (97.52%), Space: 42.5 MB (62.24%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int n) {
+        if(n<=1) return n;
+        int lhs=0, rhs=n;
+
+        int mid=n/2;
+        while(lhs<=rhs){
+            mid= (lhs)+(rhs-lhs)/2;           //(lhs+rhs)/2
+
+            if(  mid    ==  n/mid  ){         //mid*mid    ==  n
+                return mid;
+            }
+
+            else if(  mid    <  n/mid  ){
+                lhs=mid+1;
+
+            }
+            
+            else if(  mid    >  n/mid  ){
+                rhs=mid-1;
+            }
+
+        }
+        return rhs;
+    }
+}
