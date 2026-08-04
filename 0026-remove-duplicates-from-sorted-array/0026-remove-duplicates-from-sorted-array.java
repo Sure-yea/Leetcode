6b5345c25200876e9eb67fb25f03e2394c39977a
Shaurya <shaurya27112007@gmail.com>
2026-08-04 18:28:29 +0530

    Time: 1 ms (77.09%), Space: 46.6 MB (64.09%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -2,24 +2,41 @@ import java.util.Arrays;
 class Solution {
     public int removeDuplicates(int[] nums) {
 
+        // if(nums.length==0) return 0;
+        // if(nums.length==1) return 1;
+
+        // ArrayList<Integer> arr=new ArrayList<>();
+        // arr.add(nums[0]);
+
+        // int k=1;
+
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]!=arr.get(arr.size()-1)){
+        //         arr.add(nums[i]);
+        //         k++;
+        //     }
+        // }
+        // for(int i=0;i<arr.size();i++){
+        //     nums[i]=arr.get(i);
+        // }
+        
+        // return k;
+
+
         if(nums.length==0) return 0;
         if(nums.length==1) return 1;
 
-        ArrayList<Integer> arr=new ArrayList<>();
-        arr.add(nums[0]);
-
         int k=1;
-
+        int index=1;
+        int lastele=nums[0];
         for(int i=1;i<nums.length;i++){
-            if(nums[i]!=arr.get(arr.size()-1)){
-                arr.add(nums[i]);
+            if(nums[i]!=lastele){
+                nums[index]=nums[i];
+                index++;
                 k++;
             }
-        }
-        for(int i=0;i<arr.size();i++){
-            nums[i]=arr.get(i);
-        }
-        
+            lastele=nums[i];
+        }        
         return k;
     }
 }
