0a05ce15ff04541ed279fb5a78d9f2941ff6ccb6
Shaurya <shaurya27112007@gmail.com>
2026-08-06 16:28:03 +0530

    Time: 3 ms (79.74%), Space: 47.1 MB (90.13%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,6 +1,55 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-       
+        // int max=Integer.MIN_VALUE;
+        // int min=Integer.MAX_VALUE;
+        // int temp=0;
+        // int index=0;
+
+        // if(nums.length==3) return(nums[0]*nums[1]*nums[2]);
+
+        // if(nums.length==4){
+        //     int a=nums[1]*nums[2]*nums[3];
+        //     int b=nums[0]*nums[2]*nums[3];
+        //     int c=nums[0]*nums[1]*nums[3];
+        //     int d=nums[0]*nums[1]*nums[2];
+        //     return Math.max(   Math.max(  a,b   )  ,  Math.max(   c,d   )         );
+        // }
+
+
+        // for(int j=nums.length-1;j>=nums.length-3;j--){
+        //     for(int i=j;i>=0;i--){
+        //         if(max<nums[i]){
+        //             max=nums[i];
+        //             index=i;
+        //         }
+        //     }
+            
+        //     temp=nums[j];
+        //     nums[j]=nums[index];
+        //     nums[index]=temp;
+        //     max=-1001;
+            
+        // }
+        
+
+
+        // for(int j=0;j<=1;j++){
+        //     for(int i=j;i<nums.length;i++){
+        //         if(nums[i]>0){
+        //             continue;
+        //         }
+        //         if(min>nums[i]){
+        //             min=nums[i];
+        //             index=i;
+        //         }
+        //     }
+        //     if(min>0) return nums[nums.length-3]*nums[nums.length-2]*nums[nums.length-1];
+        //     temp=nums[j];
+        //     nums[j]=nums[index];
+        //     nums[index]=temp;
+        //     min=1001;
+            
+        // }
 
 
         int max=-1001;
@@ -8,7 +57,18 @@ class Solution {
         int max3=-1001;
         int min=1001;
         int min2=1001;
+        int temp=0;
+        int index=0;
 
+        // if(nums.length==3) return(nums[0]*nums[1]*nums[2]);
+
+        // if(nums.length==4){
+        //     int a=nums[1]*nums[2]*nums[3];
+        //     int b=nums[0]*nums[2]*nums[3];
+        //     int c=nums[0]*nums[1]*nums[3];
+        //     int d=nums[0]*nums[1]*nums[2];
+        //     return Math.max(   Math.max(  a,b   )  ,  Math.max(   c,d   )         );
+        // }
 
 
 
@@ -17,6 +77,7 @@ class Solution {
                 max3=max2;
                 max2=max;
                 max=nums[i];
+                index=i;
             }
             else if(max2<nums[i]){
                 max3=max2;
@@ -29,6 +90,7 @@ class Solution {
             if(min>nums[i]){
                 min2=min;
                 min=nums[i];
+                index=i;
             }
             else if(min2>nums[i]){
                 min2=nums[i];
