71347c823672b5c13b0f66af826db61f6de7f23e
Shaurya <shaurya27112007@gmail.com>
2026-08-18 17:23:08 +0530

    Time: 1 ms (3.15%), Space: 43.5 MB (43.36%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        int carry=1;
+        int i=digits.length-1;
+        int digit=digits[i];
+        
+        while(i>=0){
+            digit=digits[i];
+            digit+=carry;
+            carry=digit/10;
+            digit=digit%10;
+            arr.add(0,digit);
+            i--;
+        }
+
+        if (carry==1){
+            arr.add(0,1);
+        }
+
+
+
+        int[] ans=new int[arr.size()];
+        for(int j=0;j<arr.size();j++){
+            ans[j]=arr.get(j);
+        }
+        return ans;
+    }
+}
