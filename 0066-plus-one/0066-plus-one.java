670076392fcf2c7ba57f5a4dd5cff7cf0c64790c
Shaurya <shaurya27112007@gmail.com>
2026-08-18 17:33:51 +0530

    Time: 1 ms (3.15%), Space: 43.6 MB (24%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,23 +1,57 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        ArrayList<Integer> arr=new ArrayList<>();
-        int carry=1;
-        int i=digits.length-1;
-        int digit=digits[i];
+        // ArrayList<Integer> arr=new ArrayList<>();
+        // int carry=1;
+        // int i=digits.length-1;
+        // int digit=digits[i];
         
+        // while(i>=0){
+        //     digit=digits[i];
+        //     digit+=carry;
+        //     carry=digit/10;
+        //     digit=digit%10;
+        //     arr.add(0,digit);
+        //     i--;
+        // }
+
+        // if (carry==1){
+        //     arr.add(0,1);
+        // }
+
+
+
+        // int[] ans=new int[arr.size()];
+        // for(int j=0;j<arr.size();j++){
+        //     ans[j]=arr.get(j);
+        // }
+        // return ans;
+
+
+        ArrayList<Integer> arr=new ArrayList<>();
+    
+        int i=digits.length-1;
+        
+        while(digits[i]==9){
+            arr.add(0,0);
+            i--;
+            if(i==-1){
+                arr.add(0,1);
+                int[] ans=new int[arr.size()];
+                for(int j=0;j<arr.size();j++){
+                    ans[j]=arr.get(j);
+                }
+                return ans;
+            }
+                
+        }
+
+        arr.add(0,digits[i]+1);
+        i--;
+
         while(i>=0){
-            digit=digits[i];
-            digit+=carry;
-            carry=digit/10;
-            digit=digit%10;
-            arr.add(0,digit);
+            arr.add(0,digits[i]);
             i--;
         }
-
-        if (carry==1){
-            arr.add(0,1);
-        }
-
 
 
         int[] ans=new int[arr.size()];
