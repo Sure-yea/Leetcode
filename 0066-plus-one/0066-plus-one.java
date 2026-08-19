f8504e0c1796f209e749b02b9fd101e6d40b19c2
Shaurya <shaurya27112007@gmail.com>
2026-08-19 10:51:17 +0530

    Time: 0 ms (100%), Space: 43.2 MB (79.96%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -27,37 +27,71 @@ class Solution {
         // return ans;
 
 
-        ArrayList<Integer> arr=new ArrayList<>();
+
+
+
+        // ArrayList<Integer> arr=new ArrayList<>();
     
+        // int i=digits.length-1;
+        
+        // while(digits[i]==9){
+        //     arr.add(0);
+        //     i--;
+        //     if(i==-1){
+        //         arr.add(0,1);
+        //         int[] ans=new int[arr.size()];
+        //         for(int j=0;j<arr.size();j++){
+        //             ans[j]=arr.get(j);
+        //         }
+        //         return ans;
+        //     }
+                
+        // }
+
+        // arr.add(0,digits[i]+1);
+        // i--;
+
+        // while(i>=0){
+        //     arr.add(0,digits[i]);
+        //     i--;
+        // }
+
+
+        // int[] ans=new int[arr.size()];
+        // for(int j=0;j<arr.size();j++){
+        //     ans[j]=arr.get(j);
+        // }
+        // return ans;
+        
+        
+        int carry=1;
+
         int i=digits.length-1;
         
-        while(digits[i]==9){
-            arr.add(0,0);
-            i--;
-            if(i==-1){
-                arr.add(0,1);
-                int[] ans=new int[arr.size()];
-                for(int j=0;j<arr.size();j++){
-                    ans[j]=arr.get(j);
+        while(i>=0){
+            if(carry==1){
+                if(digits[i]==9){
+                    digits[i]=0;
+                    carry=1;
                 }
-                return ans;
+                else{
+                    digits[i]++;
+                    carry=0;
+                }
             }
+            i--;
+            
                 
         }
-
-        arr.add(0,digits[i]+1);
-        i--;
-
-        while(i>=0){
-            arr.add(0,digits[i]);
-            i--;
+        if(carry==1){
+            int[] arr=new int[digits.length+1];
+            arr[0]=1;
+            for(int a=1;a<arr.length;a++){
+                arr[a]=digits[a-1];
+                return arr;
+            }
         }
-
-
-        int[] ans=new int[arr.size()];
-        for(int j=0;j<arr.size();j++){
-            ans[j]=arr.get(j);
-        }
-        return ans;
+        
+        return digits;
     }
 }
