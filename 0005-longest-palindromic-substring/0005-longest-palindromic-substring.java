c4e6e44e656e870bc2193bcdbe1fd47ccc3b9ae2
Shaurya <shaurya27112007@gmail.com>
2026-08-20 18:14:36 +0530

    Time: 45 ms (31.27%), Space: 48.9 MB (5.37%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,29 +1,30 @@
 class Solution {
     public String longestPalindrome(String s) {
 
+        ArrayList<Character> max_palin=new ArrayList<>();
+        ArrayList<Character> palin=new ArrayList<>();
         
-        String max_palin="";
         int max=0;
         
-        String palin="";
         int i=0;
         int j=0;
         int n=s.length();
-        // if(s.length()==1) return s;
+        boolean even=false;
+
         for(int k=0;k<n;k++){
-            palin="";
-            palin+=s.charAt(k);
+            palin.clear();
+            palin.add(s.charAt(k));
             i=k-1;
             j=k+1;
             while(i>=0 && j<n && s.charAt(i)==s.charAt(j)){
-                palin=s.charAt(i)+palin+s.charAt(j);
+                palin.add(s.charAt(j));
                 i--;
                 j++;
             }
             
-            if(palin.length()>max){
-                max=palin.length();
-                max_palin=palin;
+            if(palin.size()*2-1>max){
+                max=palin.size()*2-1;
+                max_palin = new ArrayList<>(palin);
             }
             
         }
@@ -31,30 +32,70 @@ class Solution {
 
        
         
-        palin="";
+        palin.clear();
         i=0;
         j=0;
        
         for(int k=0;k<n;k++){
-            palin="";
+            palin.clear();
             
             i=k;
             j=k+1;
             while(i>=0 && j<n && s.charAt(i)==s.charAt(j)){
-                palin=s.charAt(i)+palin+s.charAt(j);
+                palin.add(s.charAt(j));
                 i--;
                 j++;
                 
             }
             
-            if(palin.length()>max){
-                max=palin.length();
-                max_palin=palin;
+            if(palin.size()*2>max){
+                max=palin.size()*2;
+                max_palin = new ArrayList<>(palin);
+                even=true;
             }
             
         }
 
-        if(max_palin.length()==0) max_palin+=s.charAt(0);
-        return max_palin;
+
+
+
+
+        String ans="";
+        int size=max_palin.size();
+
+
+        if(size==0){
+            ans+=s.charAt(0);
+            return ans;
+        }
+
+
+        else if (even==false){
+
+            for(int l=size-1;l>=0;l--){
+                ans+=max_palin.get(l);
+            }
+
+            for(int m=1;m<size;m++){
+                ans+=max_palin.get(m);
+            }
+
+        }
+        
+        else if (even==true){
+
+            for(int l=size-1;l>=0;l--){
+                ans+=max_palin.get(l);
+            }
+
+            for(int m=0;m<size;m++){
+                ans+=max_palin.get(m);
+            }
+
+        }
+
+        
+
+        return ans;
     }
 }
