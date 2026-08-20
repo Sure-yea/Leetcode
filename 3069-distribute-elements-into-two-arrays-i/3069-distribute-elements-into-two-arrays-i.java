47cd61dfb3319745b71a3d830f7f2ec9d3af5d01
Shaurya <shaurya27112007@gmail.com>
2026-08-20 23:13:49 +0530

    Time: 1 ms (98.08%), Space: 46.7 MB (42.79%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -1,20 +1,60 @@
 class Solution {
     public int[] resultArray(int[] nums) {
 
-        ArrayList<Integer> arr1 =new ArrayList<>();
-        ArrayList<Integer> arr2 =new ArrayList<>();
+        // ArrayList<Integer> arr1 =new ArrayList<>();
+        // ArrayList<Integer> arr2 =new ArrayList<>();
         
+        // int a1=nums[0];
+        
+        // int a2=nums[1];
+        // arr1.add(a1);
+        // arr2.add(a2);
+        
+        // int i=2;
+        // while(i<nums.length){
+        //     if(a1>a2){
+        //         arr1.add(nums[i]);
+        //         a1=nums[i];
+        //     }
+        //     else{
+        //         arr2.add(nums[i]);
+        //         a2=nums[i];        
+        //     }
+
+        //     i++;
+        // }
+
+        // int j=0;
+        // int[] result=new int[nums.length];
+        // for(int ele: arr1){
+        //     result[j]=ele;
+        //     j++;
+        // }
+        // for(int ele: arr2){
+        //     result[j]=ele;
+        //     j++;
+        // }
+        
+
+        // return result;
+
+    
+        ArrayList<Integer> arr2 =new ArrayList<>();
+        int[] result=new int[nums.length];
+    
         int a1=nums[0];
         
+        result[0]=nums[0];
+        int index=1;
         int a2=nums[1];
-        arr1.add(a1);
         arr2.add(a2);
         
         int i=2;
         while(i<nums.length){
             if(a1>a2){
-                arr1.add(nums[i]);
+                result[index]=nums[i];
                 a1=nums[i];
+                index++;
             }
             else{
                 arr2.add(nums[i]);
@@ -24,15 +64,9 @@ class Solution {
             i++;
         }
 
-        int j=0;
-        int[] result=new int[nums.length];
-        for(int ele: arr1){
-            result[j]=ele;
-            j++;
-        }
         for(int ele: arr2){
-            result[j]=ele;
-            j++;
+            result[index]=ele;
+            index++;
         }
         
 
