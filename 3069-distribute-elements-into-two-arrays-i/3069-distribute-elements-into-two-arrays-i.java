c8f119c8353eaf44906185b3b58e8b2714d3a44c
Shaurya <shaurya27112007@gmail.com>
2026-08-20 23:04:59 +0530

    Time: 1 ms (98.08%), Space: 46.9 MB (12.74%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+
+        ArrayList<Integer> arr1 =new ArrayList<>();
+        ArrayList<Integer> arr2 =new ArrayList<>();
+        
+        int a1=nums[0];
+        
+        int a2=nums[1];
+        arr1.add(a1);
+        arr2.add(a2);
+        
+        int i=2;
+        while(i<nums.length){
+            if(a1>a2){
+                arr1.add(nums[i]);
+                a1=nums[i];
+            }
+            else{
+                arr2.add(nums[i]);
+                a2=nums[i];        
+            }
+
+            i++;
+        }
+
+        int j=0;
+        int[] result=new int[nums.length];
+        for(int ele: arr1){
+            result[j]=ele;
+            j++;
+        }
+        for(int ele: arr2){
+            result[j]=ele;
+            j++;
+        }
+        
+
+        return result;
+    }
+}
