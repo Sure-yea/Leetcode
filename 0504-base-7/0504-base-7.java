08535993121af10cb28e256d65020128e61d6521
Shaurya <shaurya27112007@gmail.com>
2026-08-26 19:23:32 +0530

    Time: 1 ms (74.06%), Space: 42.9 MB (42.01%) - LeetHub

---
+++ 0504-base-7/0504-base-7.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String convertToBase7(int num) {
+        if(num==0) return "0";
+        ArrayList<Character> arr=new ArrayList<>();
+        boolean neg=false;
+        if(num<0){
+            num=-num;
+            neg=true;
+        }
+        
+        while(num>0){
+            arr.add((char)  ((num%7)+'0'));
+            num/=7;
+        }
+
+        if(neg==true) arr.add('-');
+
+
+        StringBuilder sb = new StringBuilder();
+        
+        int n=arr.size();
+        while(n>0){
+            sb.append(arr.get(n-1));
+            n--;
+        }
+
+        String result = sb.toString();
+        return result;
+    }
+}
